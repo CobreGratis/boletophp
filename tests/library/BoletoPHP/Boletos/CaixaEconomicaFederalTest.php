b56b8df4a73e701146532278b30cd6fa5b122c47
Mauro George <maurogot@gmail.com>
2012-07-15 11:42:13 -0300

    renomeado fixture de caixa economica federal

--- tests/library/BoletoPHP/Boletos/CaixaEconomicaFederalTest.php
+++ tests/library/BoletoPHP/Boletos/CaixaEconomicaFederalTest.php
@@ -85,6 +85,6 @@ class CaixaEconomicaFederalTest extends \PHPUnit_Framework_TestCase  {
         $this->assertEquals($view_vars['endereco1'], 'Endereço do seu Cliente');
         $this->assertEquals($view_vars['endereco1'], 'Endereço do seu Cliente');
         $this->assertEquals($view_vars['endereco2'], 'Cidade - Estado -  CEP: 00000-000');
-        $this->assertEquals($view_vars['codigo_barras'], file_get_contents('fixtures/codigo_de_barras'));
+        $this->assertEquals($view_vars['codigo_barras'], file_get_contents('fixtures/codigo_de_barras_caixa_economica_federal'));
     }
 }
