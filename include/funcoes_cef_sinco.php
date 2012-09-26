a2a928efd510025a5246343788e8423b7b3c9abb
Anderson <andersonnit@gmail.com>
2012-09-25 23:31:36 -0300

    Alterando comentários que ainda estavam no padrão iso-8859-1 para o charset UTF-8

--- include/funcoes_cef_sinco.php
+++ include/funcoes_cef_sinco.php
@@ -1,11 +1,11 @@
 <?php
 // +----------------------------------------------------------------------+
-// | BoletoPhp - Vers�o Beta                                              |
+// | BoletoPhp - Versão Beta                                              |
 // +----------------------------------------------------------------------+
-// | Este arquivo est� dispon�vel sob a Licen�a GPL dispon�vel pela Web   |
+// | Este arquivo está disponível sob a Licença GPL disponível pela Web   |
 // | em http://pt.wikipedia.org/wiki/GNU_General_Public_License           |
-// | Voc� deve ter recebido uma c�pia da GNU Public License junto com     |
-// | esse pacote; se n�o, escreva para:                                   |
+// | Você deve ter recebido uma cópia da GNU Public License junto com     |
+// | esse pacote; se não, escreva para:                                   |
 // |                                                                      |
 // | Free Software Foundation, Inc.                                       |
 // | 59 Temple Place - Suite 330                                          |
@@ -13,22 +13,20 @@
 // +----------------------------------------------------------------------+
 
 // +----------------------------------------------------------------------+
-// | Originado do Projeto BBBoletoFree que tiveram colabora��es de Daniel |
+// | Originado do Projeto BBBoletoFree que tiveram colaborações de Daniel |
 // | William Schultz e Leandro Maniezo que por sua vez foi derivado do	  |
-// | PHPBoleto de Jo�o Prado Maia e Pablo Martins F. Costa                |
+// | PHPBoleto de João Prado Maia e Pablo Martins F. Costa                |
 // |                                                                      |
 // | Se vc quer colaborar, nos ajude a desenvolver p/ os demais bancos :-)|
 // | Acesse o site do Projeto BoletoPhp: www.boletophp.com.br             |
 // +----------------------------------------------------------------------+
 
 // +----------------------------------------------------------------------+
-// | Equipe Coordena��o Projeto BoletoPhp: <boletophp@boletophp.com.br>   |
+// | Equipe Coordenação Projeto BoletoPhp: <boletophp@boletophp.com.br>   |
 // | Desenvolvimento Boleto CEF SINCO: Carlos Magno / Reinaldo Silva      |
 // +----------------------------------------------------------------------+
 
 include_once 'funcoes.php';
-
-//print "oi";
 
 
 $codigobanco = "104";
@@ -38,22 +36,22 @@ $fator_vencimento = fator_vencimento($dadosboleto["data_vencimento"]);
 
 //valor tem 10 digitos, sem virgula
 $valor = formata_numero($dadosboleto["valor_boleto"],10,0,"valor");
-//agencia � 4 digitos
+//agencia é 4 digitos
 $agencia = formata_numero($dadosboleto["agencia"],4,0);
-//conta � 5 digitos
+//conta é 5 digitos
 $conta = formata_numero($dadosboleto["conta"],5,0);
 //dv da conta
 $conta_dv = formata_numero($dadosboleto["conta_dv"],1,0);
-//carteira � 2 caracteres
+//carteira é 2 caracteres
 $carteira = $dadosboleto["carteira"];
 
-//nosso n�mero (sem dv) � 17 digitos
+//nosso número (sem dv) é 17 digitos
 $nnum = $dadosboleto["inicio_nosso_numero"] . formata_numero($dadosboleto["nosso_numero"],17,0);
-//dv do nosso n�mero
+//dv do nosso número
 $dv_nosso_numero = digitoVerificador_nossonumero($nnum);
 $nossonumero_dv ="$nnum$dv_nosso_numero";
 
-//conta cedente (sem dv) � 6 digitos
+//conta cedente (sem dv) é 6 digitos
 $conta_cedente = formata_numero($dadosboleto["conta_cedente"],6,0);
 //dv da conta cedente
 $conta_cedente_dv = formata_numero($dadosboleto["conta_cedente_dv"],1,0);
@@ -88,7 +86,7 @@ function digitoVerificador_nossonumero($numero) {
 	 return $dv;
 }
 
-// FUN��ES
+// FUNÇÕES
 // Algumas foram retiradas do Projeto PhpBoleto e modificadas para atender as particularidades de cada banco
 
 function geraCodigoBanco($numero) {
