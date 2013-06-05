0c9a9525e54b7471763796bc650031bf96c046a2
RAFFAELCAMPOS <rafaoi4@hotmail.com>
2013-06-05 12:03:35 -0300

    Update boleto_itau.php

--- boleto_itau.php
+++ boleto_itau.php
@@ -1,11 +1,11 @@
 <?php
 // +----------------------------------------------------------------------+
-// | BoletoPhp - Versão Beta                                              |
+// | BoletoPhp - Versï¿½o Beta                                              |
 // +----------------------------------------------------------------------+
-// | Este arquivo está disponível sob a Licença GPL disponível pela Web   |
+// | Este arquivo estï¿½ disponï¿½vel sob a Licenï¿½a GPL disponï¿½vel pela Web   |
 // | em http://pt.wikipedia.org/wiki/GNU_General_Public_License           |
-// | Você deve ter recebido uma cópia da GNU Public License junto com     |
-// | esse pacote; se não, escreva para:                                   |
+// | Vocï¿½ deve ter recebido uma cï¿½pia da GNU Public License junto com     |
+// | esse pacote; se nï¿½o, escreva para:                                   |
 // |                                                                      |
 // | Free Software Foundation, Inc.                                       |
 // | 59 Temple Place - Suite 330                                          |
@@ -13,79 +13,69 @@
 // +----------------------------------------------------------------------+
 
 // +----------------------------------------------------------------------+
-// | Originado do Projeto BBBoletoFree que tiveram colaborações de Daniel |
+// | Originado do Projeto BBBoletoFree que tiveram colaboraï¿½ï¿½es de Daniel |
 // | William Schultz e Leandro Maniezo que por sua vez foi derivado do	  |
-// | PHPBoleto de João Prado Maia e Pablo Martins F. Costa				        |
+// | PHPBoleto de Joï¿½o Prado Maia e Pablo Martins F. Costa				        |
 // | 														                                   			  |
 // | Se vc quer colaborar, nos ajude a desenvolver p/ os demais bancos :-)|
 // | Acesse o site do Projeto BoletoPhp: www.boletophp.com.br             |
 // +----------------------------------------------------------------------+
 
 // +----------------------------------------------------------------------+
-// | Equipe Coordenação Projeto BoletoPhp: <boletophp@boletophp.com.br>   |
-// | Desenvolvimento Boleto Itaú: Glauber Portella                        |
+// | Equipe Coordenaï¿½ï¿½o Projeto BoletoPhp: <boletophp@boletophp.com.br>   |
+// | Desenvolvimento Boleto Itaï¿½: Glauber Portella                        |
 // +----------------------------------------------------------------------+
 
 
-// ------------------------- DADOS DINÂMICOS DO SEU CLIENTE PARA A GERAÇÃO DO BOLETO (FIXO OU VIA GET) -------------------- //
-// Os valores abaixo podem ser colocados manualmente ou ajustados p/ formulário c/ POST, GET ou de BD (MySql,Postgre,etc)	//
+// ------------------------- DADOS DINï¿½MICOS DO SEU CLIENTE PARA A GERAï¿½ï¿½O DO BOLETO (FIXO OU VIA GET) -------------------- //
+// Os valores abaixo podem ser colocados manualmente ou ajustados p/ formulï¿½rio c/ POST, GET ou de BD (MySql,Postgre,etc)	//
 
 // DADOS DO BOLETO PARA O SEU CLIENTE
-$dias_de_prazo_para_pagamento = 5;
+$dias_de_prazo_para_pagamento = 30;
 $taxa_boleto = 2.95;
-$data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
-$valor_cobrado = "2950,00"; // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+$data_venc = date("07/08/2012", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
+$valor_cobrado = "381,55"; // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
 $valor_cobrado = str_replace(",", ".",$valor_cobrado);
 $valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
 
-$dadosboleto["nosso_numero"] = '12345678';  // Nosso numero - REGRA: Máximo de 8 caracteres!
-$dadosboleto["numero_documento"] = '0123';	// Num do pedido ou nosso numero
+$dadosboleto["nosso_numero"] = '00425678';  // Nosso numero - REGRA: Mï¿½ximo de 8 caracteres!
+$dadosboleto["numero_documento"] = '0524';	// Num do pedido ou nosso numero
 $dadosboleto["data_vencimento"] = $data_venc; // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
-$dadosboleto["data_documento"] = date("d/m/Y"); // Data de emissão do Boleto
-$dadosboleto["data_processamento"] = date("d/m/Y"); // Data de processamento do boleto (opcional)
-$dadosboleto["valor_boleto"] = $valor_boleto; 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
+$dadosboleto["data_documento"] = date("29/6/2012"); // Data de emissï¿½o do Boleto
+$dadosboleto["data_processamento"] = date("29/6/2012"); // Data de processamento do boleto (opcional)
+$dadosboleto["valor_boleto"] = $valor_boleto; 	// Valor do Boleto - REGRA: Com vï¿½rgula e sempre com duas casas depois da virgula
 
 // DADOS DO SEU CLIENTE
-$dadosboleto["sacado"] = "Nome do seu Cliente";
-$dadosboleto["endereco1"] = "Endereço do seu Cliente";
-$dadosboleto["endereco2"] = "Cidade - Estado -  CEP: 00000-000";
+$dadosboleto["sacado"] = "CONSTRUTORA INCORPORADORA TRADIÃ‡AO";
+$dadosboleto["endereco1"] = "AV GETULIO VARGAS 615-S";
+$dadosboleto["endereco2"] = "CHAPECÃ“ - SC -  CEP: 89812120";
 
 // INFORMACOES PARA O CLIENTE
-$dadosboleto["demonstrativo1"] = "Pagamento de Compra na Loja Nonononono";
-$dadosboleto["demonstrativo2"] = "Mensalidade referente a nonon nonooon nononon<br>Taxa bancária - R$ ".number_format($taxa_boleto, 2, ',', '');
-$dadosboleto["demonstrativo3"] = "BoletoPhp - http://www.boletophp.com.br";
-$dadosboleto["instrucoes1"] = "- Sr. Caixa, cobrar multa de 2% após o vencimento";
-$dadosboleto["instrucoes2"] = "- Receber até 10 dias após o vencimento";
-$dadosboleto["instrucoes3"] = "- Em caso de dúvidas entre em contato conosco: xxxx@xxxx.com.br";
-$dadosboleto["instrucoes4"] = "&nbsp; Emitido pelo sistema Projeto BoletoPhp - www.boletophp.com.br";
-
-// DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
-$dadosboleto["quantidade"] = "";
-$dadosboleto["valor_unitario"] = "";
-$dadosboleto["aceite"] = "";		
-$dadosboleto["especie"] = "R$";
-$dadosboleto["especie_doc"] = "";
+$dadosboleto["demonstrativo1"] = "Pagamento de manutenÃ§Ã£o de elevador";
+$dadosboleto["instrucoes1"] = "- Sr. Caixa, cobrar multa de 2% apï¿½s o vencimento";
+$dadosboleto["instrucoes2"] = "- Receber atï¿½ 1 dia sapï¿½s o vencimento";
+$dadosboleto["instrucoes3"] = "- ApÃ³s 5 dias do vencimento irÃ¡ para protesto";
 
 
-// ---------------------- DADOS FIXOS DE CONFIGURAÇÃO DO SEU BOLETO --------------- //
+// ---------------------- DADOS FIXOS DE CONFIGURAï¿½ï¿½O DO SEU BOLETO --------------- //
 
 
-// DADOS DA SUA CONTA - ITAÚ
+// DADOS DA SUA CONTA - ITAï¿½
 $dadosboleto["agencia"] = "1565"; // Num da agencia, sem digito
 $dadosboleto["conta"] = "13877";	// Num da conta, sem digito
 $dadosboleto["conta_dv"] = "4"; 	// Digito do Num da conta
 
-// DADOS PERSONALIZADOS - ITAÚ
-$dadosboleto["carteira"] = "175";  // Código da Carteira: pode ser 175, 174, 104, 109, 178, ou 157
+// DADOS PERSONALIZADOS - ITAï¿½
+$dadosboleto["carteira"] = "175";  // Cï¿½digo da Carteira: pode ser 175, 174, 104, 109, 178, ou 157
 
 // SEUS DADOS
-$dadosboleto["identificacao"] = "BoletoPhp - Código Aberto de Sistema de Boletos";
-$dadosboleto["cpf_cnpj"] = "";
-$dadosboleto["endereco"] = "Coloque o endereço da sua empresa aqui";
-$dadosboleto["cidade_uf"] = "Cidade / Estado";
-$dadosboleto["cedente"] = "Coloque a Razão Social da sua empresa aqui";
+$dadosboleto["identificacao"] = "ELEVADORES OTIS LTDA;
+$dadosboleto["cpf_cnpj"] = "29.739.737/0008-89";
+$dadosboleto["endereco"] = "R WENCESLAU GLASER 162";
+$dadosboleto["cidade_uf"] = "CURITIBA / SC";
+$dadosboleto["cedente"] = "ELEVADORES OTIS LTDA";
 
-// NÃO ALTERAR!
+// Nï¿½O ALTERAR!
 include("include/funcoes_itau.php"); 
 include("include/layout_itau.php");
 ?>
